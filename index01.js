e47349e9381655fe2846587d018e267006406a80
shankarsanti <shankarsanti2005@gmail.com>
2025-11-11 11:44:30 +0530

    basic js

---
+++ index01.js
@@ -0,0 +1,18 @@
+console.log("Hello World");
+console.log("Welcome to my website");
+
+
+window.alert("Welcome to my website!");
+window.alert("Enjoy your stay!");
+
+
+document.getElementById("MYH1").innerHTML = "HELLO WORLD!";
+document.getElementById("myp").innerHTML = "This is my first website.";
+
+
+//this is a  comment
+
+/* this
+ is
+a 
+comment */
