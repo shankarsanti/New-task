d5fb14cd8cfad459dfc65779013d9c6fb5ed98ae
shankarsanti <shankarsanti2005@gmail.com>
2025-11-17 15:22:22 +0530

    details

---
+++ data type/index.js
@@ -0,0 +1,24 @@
+function profile()
+{
+    username="shankar"; //string data type
+    age=21; //number data type
+    classname="BCA"; //string data type
+    marks=true; //boolean data type
+     profileData;
+    time=null; //null data type
+
+}
+
+
+
+
+
+console.log("profile");
+console.log(profile);
+
+
+  
+    console.log(typeof profile);
+    console.log(typeof ["profile.username"]);
+
+
