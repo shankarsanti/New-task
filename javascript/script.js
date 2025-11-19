1d98f31b8ffefd28361c4bf09b4a258c7c82d4ac
shankarsanti <shankarsanti2005@gmail.com>
2025-11-19 14:45:43 +0530

    script

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let a = 5;
+let b = 10;
+/*console.log("a + b =", a + b);
+console.log("a - b =", b - a);
+console.log("a * b =", a * b);
+console.log("b / a =", b / a);
+console.log("b % a =", b % a);*/
+
+
+console.log("a ** 2 =", a ** 2);
+console.log("b ** 2 =", b ** 2);
+console.log("Increment a:", ++a);
+console.log("Decrement b:", --b);
+console.log("Final values - a:", a, ", b:", b);
