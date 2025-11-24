fdb803dcc6c057bd4fe62e86e7eff009ce9e4ac8
shankarsanti <shankarsanti2005@gmail.com>
2025-11-24 11:43:54 +0530

    operators

---
+++ operators/optr.js
@@ -0,0 +1,24 @@
+let a = 5;
+let b = 10;
+
+
+console.log("a += 3:", a += 3);
+console.log("b -= 2:", b -= 2);
+console.log("a *= 4:", a *= 4);
+console.log("b /= 2:", b /= 2);
+console.log("a %= 5:", a %= 5);
+console.log("Final values - a:", a, ", b:", b);
+console.log("a **= 3:", a **= 3);
+
+
+console.log("a == 3:", a == 3);
+console.log("b != 4:", b != 4);
+console.log("a === 27:", a === 27);
+console.log("b !== 4:", b !== 4);
+
+
+
+console.log("a > 2:", a > 2);
+console.log("b < 5:", b < 5);
+console.log("a >= 27:", a >= 27);
+console.log("b <= 4:", b <= 4);
---
+++ operators/script.js
@@ -0,0 +1,30 @@
+//let a = 5;
+//let b = 10;
+
+
+
+console.log("a + b =", a + b);
+console.log("a - b =", b - a);
+console.log("a * b =", a * b);
+console.log("b / a =", b / a);
+console.log("b % a =", b % a);
+
+
+console.log("a ** 2 =", a ** 2);
+console.log("b ** 2 =", b ** 2);
+console.log("Increment a:", ++a);
+console.log("Decrement b:", --b);
+console.log("Final values - a:", a, ", b:", b);
+
+let a = 5;
+let b = 10;
+
+
+console.log("a += 3:", a += 3);
+console.log("b -= 2:", b -= 2);
+console.log("a *= 4:", a *= 4);
+console.log("b /= 2:", b /= 2);
+console.log("a %= 5:", a %= 5);
+console.log("Final values - a:", a, ", b:", b);
+console.log("a **= 3:", a **= 3);
+
