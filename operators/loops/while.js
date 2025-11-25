e4016c0803f6bbe8543bc427bc53c8bf73106c53
shankarsanti <shankarsanti2005@gmail.com>
2025-11-25 16:03:14 +0530

    loops

---
+++ operators/loops/while.js
@@ -0,0 +1,37 @@
+let age = 20;
+while (age <= 18) {
+    console.log("You are not allowed to vote");
+    age++;
+}
+console.log("You are allowed to vote");
+
+
+
+let number = 1; 
+while (number <= 10) {
+    console.log(number);
+    number++;
+}
+
+
+
+let count = 10;
+while (count >= 1) {
+    console.log(count);
+    count--;
+}
+
+
+let num = 1;
+do {
+    console.log(num);
+    num++;
+} while (num <= 5);
+
+
+let n = 5;
+do {
+    console.log(n);
+    n--;
+} while (n >= 1);
+
