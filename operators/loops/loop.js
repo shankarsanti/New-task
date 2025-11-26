7b8f351aa4e9d9f1dbf566838bdddac420210379
shankarsanti <shankarsanti2005@gmail.com>
2025-11-26 11:34:19 +0530

    practice

--- operators/loops/loop.js
+++ operators/loops/loop.js
@@ -21,7 +21,42 @@ let person = {
     age: 30,
     city: "New York"
 };  for ( let key in person ){
-    console.log("key",key, person[key]);
+    console.log(key, person[key]);
 }   
 
 
+
+let student = {
+    name: "Alice",
+    age: 22,
+    major: "Computer Science"
+};
+ for ( let key in student ){
+    console.log(key, student[key]);
+}
+
+
+let fruits = ["apple", "banana", "cherry"];
+for ( let fruit of fruits ){
+    console.log("fruit", fruit);
+}
+
+let total = 0;
+let prices = [10, 20, 15, 25];
+for ( let price of prices ){
+    total += price;
+}
+console.log("total price", total);
+
+
+let boys = {
+    name: "Bob",
+    age: 24,
+    city: "Los Angeles"
+};
+ for ( let key in boys ){
+    console.log(key,boys[key]);
+}
+for ( let key of boys) {
+    console.log(key);
+}
---
+++ operators/loops/practice.js
@@ -0,0 +1,29 @@
+// practice 1
+for(num = 0; num <= 100; num++){
+    if (num %2===0){
+    console.log("num", num);
+    }
+}
+console.log("the total even numbers", num);
+
+let numbers;
+for (num =0; num <= 100; num++){
+    if (num %2 !==0){
+        console.log("num", num);
+    }  
+    total number= numbers + num;
+}console.log("the total odd numbers", numbers);
+
+
+
+
+// practice 2
+let gamenumber = 14;
+let usernumber = prompt("enter your numbers ,to gess the game number");
+
+
+while (usernumber != gamenumber){
+    usernumber = prompt ("wrong gess , please try again");
+}
+ console.log("congratulation! you gess the right number");
+ console.log ("thank you ");
