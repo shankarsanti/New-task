4530ec035a5b30bd28bd73b850d00a32f4596331
shankarsanti <shankarsanti2005@gmail.com>
2025-11-29 16:32:39 +0530

    functions

---
+++ operators/functions/fun.js
@@ -0,0 +1,38 @@
+// function myfunction(){
+//     console.log("i love js")
+//     console.log("functions for javascript")
+//     console.log("js functions")
+// }
+
+// myfunction();
+// myfunction();
+// myfunction();
+
+
+
+
+// function sum(x,y){
+//     sum=x+y;
+//     return sum;
+// }
+// let value=10+5;
+// console.log(value);
+
+
+// function sum(a+b){
+//     return a+b;
+// }
+//     const arrowsum=(a+b)=> {
+//         console.log(a+b);
+//     }
+
+
+// function mul(a*b){
+//     return a*b;
+// }
+// const arrowmul(a*b)=>{
+//     return a*b;
+//     console,log(a*b){
+// }
+         
+     
