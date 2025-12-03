0bdf3546cc29797a6a0e1f2ee590dc0356b6ad9b
shankarsanti <shankarsanti2005@gmail.com>
2025-12-03 16:55:01 +0530

    nighan2

--- operators/functions/fun.js
+++ operators/functions/fun.js
@@ -19,20 +19,45 @@
 // console.log(value);
 
 
-// function sum(a+b){
-//     return a+b;
-// }
-//     const arrowsum=(a+b)=> {
-//         console.log(a+b);
-//     }
+// // function sum(a+b){
+// //     return a+b;
+// // }
+// //     const arrowsum=(a+b)=> {
+// //         console.log(a+b);
+// //     }
 
 
-// function mul(a*b){
-//     return a*b;
-// }
-// const arrowmul(a*b)=>{
-//     return a*b;
-//     console,log(a*b){
-// }
+
          
-     
+//      const printhello=()=>{
+//         console.log("hello");
+//      }
+
+
+//      function countvowels(str){
+//         count = 0;
+//         for(const char of str){
+//             if(
+//                 char===a||
+//                 char===e||
+//                 char===i||
+//                 char===o||
+//                 char===u)
+//                 {
+//                     count ++;
+//                 }
+//         }
+//         return count;
+//      }
+
+
+    //  let arr=[1,3,4,9,11]
+    //  arr.forEach(function printvalue(val){
+    //  console.log(val);
+    //  }); 
+ 
+ 
+    let arr=[11,12,13,14,15]
+     arr (12) => {
+        console.log(arr)
+     }
